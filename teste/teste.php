5b17e866a546f3c04276a988a6ec5c90154c82f3
pietrorpedro <pietrorpedro@gmail.com>
2022-11-08 12:02:35 -0300

    testes

--- teste/teste.php
+++ teste/teste.php
@@ -1,17 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Pokedex</title>
   <link rel="stylesheet" href="teste.css">
 </head>
 <body>
-  <div class="book">
-    <div class="back"></div>
-    <div class="page1"></div>
-    <div class="front"></div>
+  <div class="flex">
+    <div class="capa">CAPA</div>
+    <div class="block">TESTE</div>
   </div>
 </body>
 </html>
