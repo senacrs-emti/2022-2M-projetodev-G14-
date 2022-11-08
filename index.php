ee06ea54021e051867b85a9cfc9686174474201e
pietrorpedro <pietrorpedro@gmail.com>
2022-11-08 11:14:57 -0300

    at

--- index.php
+++ index.php
@@ -104,19 +104,19 @@
                 <strong></strong><br/>
             </div>
             <div id="blueButtons1">
-            <div class="blueButton"></div>
-            <div class="blueButton"></div>
+                <div class="blueButton"></div>
+                <div class="blueButton"></div>
             <div class="blueButton"></div>
             <div class="blueButton"></div>
             <div class="blueButton"></div>
             </div>
             <div id="blueButtons2">
+                <div class="blueButton"></div>
             <div class="blueButton"></div>
             <div class="blueButton"></div>
             <div class="blueButton"></div>
             <div class="blueButton"></div>
-            <div class="blueButton"></div>
-            </div>
+        </div>
             <div id="miniButtonGlass4"></div>
             <div id="miniButtonGlass5"></div>
             <div id="barbutton3"></div>
@@ -128,6 +128,7 @@
             <div id="curve1_right"></div>
             <div id="curve2_right"></div>
         </div>
+        <div class="page turn" id="red">RED</div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     <script src="main.js"></script>
--- teste/teste.php
+++ teste/teste.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="teste.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="teste.css">
 </head>
 <body>
   <div class="book">
-    <span class="page turn">a</span>
-    <span class="cover turn">b</span>
-    <div class="seila">aa</div>
+    <div class="back"></div>
+    <div class="page1"></div>
+    <div class="front"></div>
   </div>
 </body>
 </html>
