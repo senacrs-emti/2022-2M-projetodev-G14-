2780f3b109202a20fb617ee97affb8699ea4feba
IPreHandaI <yanaluiza26y@gmail.com>
2022-11-24 11:43:21 -0300

    testepreta

--- index.js
+++ index.js
@@ -3,4 +3,17 @@ const inputPokemonNome = document.getElementById("inputPokemonNome")
 
 botaoIniciar.addEventListener("click", () => {
     inputPokemonNome.focus()
-})
+});
+
+
+$(document).ready(function() {
+
+    $('#bigbluebutton').click(function(){
+        
+            success: function(results){
+                $(".tela").html(results);
+            }
+        });
+    });
+
+});
