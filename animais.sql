0b96c7ed77aabc3b40e1347729e9e90da054fa84
pietrorpedro <pietrorpedro@gmail.com>
2022-12-01 10:59:46 -0300

    Update animais.sql

--- animais.sql
+++ animais.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 30-Nov-2022 às 23:54
--- Versão do servidor: 10.4.24-MariaDB
--- versão do PHP: 8.1.6
+-- Tempo de geração: 01-Dez-2022 às 14:59
+-- Versão do servidor: 10.4.17-MariaDB
+-- versão do PHP: 7.3.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -105,75 +105,75 @@ CREATE TABLE `gatos` (
 --
 
 INSERT INTO `gatos` (`ID`, `nome`, `caracteristicas`, `imagem`) VALUES
-(1, 'Seilao', 'Ativo agil brincalhao', 'https://qcanimais.com.br/wp-content/uploads/2018/08/ceylon1.jpg'),
+(1, 'Seilao', 'Ativo agil brincalhão', 'https://qcanimais.com.br/wp-content/uploads/2018/08/ceylon1.jpg'),
 (2, 'Highland', 'Cativante doce afetuoso', 'https://meusanimais.com.br/wp-content/uploads/2021/08/gato-highland-fold-amarelo-768x576.jpg'),
 (3, 'Minskin', 'Alegres carinhosos brincalhoes', 'https://t2.ea.ltmcdn.com/pt/razas/5/2/8/gato-minskin_825_0_600.jpg'),
 (4, 'Ural rex', 'Carinhoso social inteligente', 'https://thumbs.dreamstime.com/b/o-gato-encaracolado-ador%C3%A1vel-ural-rex-est%C3%A1-e-olha-para-frente-com-olhos-verdes-propriet%C3%A1rio-145231664.jpg'),
 (5, 'Colopoint', 'Inteligente ativo afetuoso', 'http://amigopet.org/wp-content/uploads/2021/03/Gato-Colorpoint01.jpg'),
 (6, 'Neva masquerade', 'Grande carinhoso ativo', 'https://www.zooplus.pt/magazine/wp-content/uploads/2018/06/neva-masquerade-768x621.jpg'),
-(7, 'Chantilly-tiffany', 'Adoravel simpatico fofo', 'https://i0.statig.com.br/bancodeimagens/f2/mq/98/f2mq98dyiiz2k3f9p8b507fs5.jpg'),
-(8, 'Kurilian bobtail', 'Simpatico ativo inteligente', 'https://i0.statig.com.br/bancodeimagens/5n/ks/u9/5nksu9fw08mkmiahadbx9zyh7.jpg'),
-(9, 'Rex alemao', 'Carinhosos sociaveis doceis', 'https://www.enciclopet.com.br/wp-content/uploads/2016/09/german-rex.jpg'),
+(7, 'Chantilly-tiffany', 'Adorável simpático fofo', 'https://i0.statig.com.br/bancodeimagens/f2/mq/98/f2mq98dyiiz2k3f9p8b507fs5.jpg'),
+(8, 'Kurilian bobtail', 'Simpático ativo inteligente', 'https://i0.statig.com.br/bancodeimagens/5n/ks/u9/5nksu9fw08mkmiahadbx9zyh7.jpg'),
+(9, 'Rex alemao', 'Carinhosos sociáveis doceis', 'https://www.enciclopet.com.br/wp-content/uploads/2016/09/german-rex.jpg'),
 (10, 'Sphynx', 'Carinhoso amigavel inteligente', 'https://www.petlove.com.br/images/breeds/192403/profile/original/sphynx-p.jpg?1532626992'),
-(11, 'Levkoy', 'Alegre sociavel afetuoso', 'https://t1.ea.ltmcdn.com/pt/razas/9/8/7/gato-levkoy-ucraniano_789_0_600.jpg'),
-(12, 'Elfo', 'Simpatico ativo inteligente', 'https://meusanimais.com.br/wp-content/uploads/2018/04/gato-elfo-origem.jpg'),
-(13, 'Bambino', 'Afetuoso ativo brincalhao', 'https://t2.ea.ltmcdn.com/pt/razas/4/8/7/gato-bambino_784_0_600.jpg'),
+(11, 'Levkoy', 'Alegre sociável afetuoso', 'https://t1.ea.ltmcdn.com/pt/razas/9/8/7/gato-levkoy-ucraniano_789_0_600.jpg'),
+(12, 'Elfo', 'Simpático ativo inteligente', 'https://meusanimais.com.br/wp-content/uploads/2018/04/gato-elfo-origem.jpg'),
+(13, 'Bambino', 'Afetuoso ativo brincalhão', 'https://t2.ea.ltmcdn.com/pt/razas/4/8/7/gato-bambino_784_0_600.jpg'),
 (14, 'Lykoi', 'Doces amistosos brincalhoes', 'https://s2.glbimg.com/HtygRNJSOz0wh9pSjFYslRMNZT8=/0x0:596x558/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_fb623579cd474803aedbbbbae014af68/internal_photos/bs/2022/o/J/bsXOYRRJmXrP3jpbH4XQ/2022-07-26-1-ja-ouviu-falar-em-gato-lobisomem-conheca-o-lykoi-felino-domestico-que-lembra-o-ser-lendario.jpeg'),
-(15, 'Ragamuffin', 'Afetuoso sociavel docil', 'https://t2.ea.ltmcdn.com/pt/razas/7/4/7/gato-ragamuffin_747_0_600.jpg'),
-(16, 'British longhair', 'Independente afetuoso brincalhao', 'https://t2.ea.ltmcdn.com/pt/razas/3/4/7/gato-british-longhair_743_0_600.jpg'),
+(15, 'Ragamuffin', 'Afetuoso sociável dócil', 'https://t2.ea.ltmcdn.com/pt/razas/7/4/7/gato-ragamuffin_747_0_600.jpg'),
+(16, 'British longhair', 'Independente afetuoso brincalhão', 'https://t2.ea.ltmcdn.com/pt/razas/3/4/7/gato-british-longhair_743_0_600.jpg'),
 (17, 'Caracat', 'Inquietos energicos brincalhoes', 'https://t1.ea.ltmcdn.com/pt/razas/9/2/7/gato-caracat_729_0_orig.jpg'),
-(18, 'Maine coon', ' gande docil gentil', 'https://static1.patasdacasa.com.br/articles/5/26/35/@/11233-os-gatos-da-raca-maine-coon-podem-parece-articles_media_mobile-1.jpg'),
+(18, 'Maine coon', ' grande dócil gentil', 'https://static1.patasdacasa.com.br/articles/5/26/35/@/11233-os-gatos-da-raca-maine-coon-podem-parece-articles_media_mobile-1.jpg'),
 (19, 'Khao manee', 'Afetuoso ativo inquieto', 'https://static1.patasdacasa.com.br/articles/5/17/65/@/8431-khao-manee-voce-ja-ouviu-falar-nessa-ra-articles_media_mobile-2.jpg'),
-(20, 'Bobtail americano', 'Energico brincalhao carinhoso', 'https://t1.ea.ltmcdn.com/pt/razas/6/1/7/gato-bobtail-americano_716_0_orig.jpg'),
+(20, 'Bobtail americano', 'Energico brincalhão carinhoso', 'https://t1.ea.ltmcdn.com/pt/razas/6/1/7/gato-bobtail-americano_716_0_orig.jpg'),
 (21, 'Singapura', 'Tranquilo inteligente', 'https://www.petlove.com.br/images/breeds/226682/profile/original/singapura-petlove-perfil.jpg?1596652984'),
-(22, 'Cymric', 'Carinhoso simpatico sociavel', 'https://www.purina.pt/sites/default/files/styles/ttt_image_510/public/2021-02/CAT%20BREED%20Hero%20Mobile_0031_Cymrics.jpg?itok=LQSdU3tH'),
-(23, 'Skookum', 'Afetuoso leal sociavel', 'https://t1.ea.ltmcdn.com/pt/razas/4/1/7/gato-skookum_714_0_600.jpg'),
-(24, 'Bobtail japones', 'Alegre ativo brincalhao', 'https://t2.ea.ltmcdn.com/pt/razas/1/1/7/gato-bobtail-japones_711_0_orig.jpg'),
-(25, 'Toyger', 'Brincalhao carinhoso curioso', 'https://t1.ea.ltmcdn.com/pt/razas/8/9/6/gato-toyger_698_0_600.jpg'),
-(26, 'America wirehair', 'Adoravel lindo leal', 'https://t2.ea.ltmcdn.com/pt/razas/5/6/6/gato-american-wirehair_665_0_orig.jpg'),
+(22, 'Cymric', 'Carinhoso simpático sociável', 'https://www.purina.pt/sites/default/files/styles/ttt_image_510/public/2021-02/CAT%20BREED%20Hero%20Mobile_0031_Cymrics.jpg?itok=LQSdU3tH'),
+(23, 'Skookum', 'Afetuoso leal sociável', 'https://t1.ea.ltmcdn.com/pt/razas/4/1/7/gato-skookum_714_0_600.jpg'),
+(24, 'Bobtail japones', 'Alegre ativo brincalhão', 'https://t2.ea.ltmcdn.com/pt/razas/1/1/7/gato-bobtail-japones_711_0_orig.jpg'),
+(25, 'Toyger', 'Brincalhão carinhoso curioso', 'https://t1.ea.ltmcdn.com/pt/razas/8/9/6/gato-toyger_698_0_600.jpg'),
+(26, 'America wirehair', 'Adorável lindo leal', 'https://t2.ea.ltmcdn.com/pt/razas/5/6/6/gato-american-wirehair_665_0_orig.jpg'),
 (27, 'Burmilla', 'Atento afetuoso apegado', 'https://static1.patasdacasa.com.br/articles/6/28/26/@/11835-gato-burmilla-conheca-mais-sobre-essa-r-opengraph_1200-3.jpg'),
 (28, 'Pixie-bob', 'Amigavel amoroso paciente', 'https://www.petlove.com.br/images/breeds/226284/profile/original/pixie-bob-perfil-petlove.jpg?1596149122'),
-(29, 'American curl', 'Afetuoso socuavel amavel', 'https://www.petlove.com.br/images/breeds/199525/profile/original/american-curl-perfil-petlove.jpg?1613743699'),
-(30, 'Laperm', 'Docil afetuoso', 'https://t2.ea.ltmcdn.com/pt/razas/5/5/6/gato-laperm_655_0_orig.jpg'),
+(29, 'American curl', 'Afetuoso socuavel amável', 'https://www.petlove.com.br/images/breeds/199525/profile/original/american-curl-perfil-petlove.jpg?1613743699'),
+(30, 'Laperm', 'Dócil afetuoso', 'https://t2.ea.ltmcdn.com/pt/razas/5/5/6/gato-laperm_655_0_orig.jpg'),
 (31, 'Tonquines', 'Doce carinhoso inquieto', 'https://www.racoesreis.com.br/wordpress/wp-content/uploads/imagem_do_post-52.jpg'),
 (32, 'Javanes', 'Carinhosos comunicativos cativantes', 'https://t1.ea.ltmcdn.com/pt/razas/3/2/6/gato-javanes_623_0_orig.jpg'),
 (33, 'Somali', 'Inteligente impotente elegante', 'https://www.petlove.com.br/images/breeds/226885/mobile_header/original/somali-perfil.jpg?1596822504'),
 (34, 'Chausie', 'Inteligente teimoso fiel', 'https://t2.ea.ltmcdn.com/pt/razas/0/2/6/gato-chausie_620_0_orig.jpg'),
-(35, 'Birmanes', 'Brincalhao curioso comunicativo', 'https://static1.patasdacasa.com.br/articles/9/28/59/@/11940-o-gato-birmanes-fara-voce-se-apaixonar-c-articles_media_mobile-1.jpg'),
-(36, 'Sagrado da birmania', 'Docil tranquilo', 'https://www.petlove.com.br/images/breeds/193051/profile/original/sagrado_da_birmania-p.jpg?1532626931'),
+(35, 'Birmanes', 'Brincalhão curioso comunicativo', 'https://static1.patasdacasa.com.br/articles/9/28/59/@/11940-o-gato-birmanes-fara-voce-se-apaixonar-c-articles_media_mobile-1.jpg'),
+(36, 'Sagrado da birmania', 'Dócil tranquilo', 'https://www.petlove.com.br/images/breeds/193051/profile/original/sagrado_da_birmania-p.jpg?1532626931'),
 (37, 'Sosoke', 'Amigavel peculiar energico', 'https://t1.ea.ltmcdn.com/pt/razas/7/1/6/gato-sokoke_617_0_orig.jpg'),
-(38, 'Davon rex', 'Carinhoso companheiro brincalhao', 'https://t2.ea.ltmcdn.com/pt/razas/9/0/6/gato-devon-rex_609_0_600.jpg'),
-(39, 'Turkish van', 'Sociavel cativante lindo', 'https://static1.patasdacasa.com.br/articles/1/16/91/@/8076-turkish-van-ou-van-turco-conheca-mais-s-opengraph_1200-2.jpg'),
+(38, 'Davon rex', 'Carinhoso companheiro brincalhão', 'https://t2.ea.ltmcdn.com/pt/razas/9/0/6/gato-devon-rex_609_0_600.jpg'),
+(39, 'Turkish van', 'Sociável cativante lindo', 'https://static1.patasdacasa.com.br/articles/1/16/91/@/8076-turkish-van-ou-van-turco-conheca-mais-s-opengraph_1200-2.jpg'),
 (40, 'Korat', 'Companheiro carinhoso tranquilo', 'https://royalpets.vteximg.com.br/arquivos/ids/177834/korat-500-500-1.jpg?v=637534859391200000'),
 (41, 'Savannah', 'Ativo social carinhoso', 'https://static.wixstatic.com/media/545b39_25e720cd008d4be4bba2be90d8299dc1.jpg/v1/fill/w_600,h_450,al_c,lg_1,q_80,enc_auto/545b39_25e720cd008d4be4bba2be90d8299dc1.jpg'),
 (42, 'Oriental shorthair', 'Extrovertido afetuoso', 'https://www.petlove.com.br/images/breeds/226251/profile/original/oriental-perfil-petlove.jpg?1596138291'),
-(43, 'Chartreux', 'Adoravel docil carinhoso', 'https://guiaanimal.net/uploads/article/image/221/AdobeStock_398113113.jpeg'),
+(43, 'Chartreux', 'Adorável dócil carinhoso', 'https://guiaanimal.net/uploads/article/image/221/AdobeStock_398113113.jpeg'),
 (44, 'Selkirk rex', 'Tranquilo paciente', 'https://t2.ea.ltmcdn.com/pt/razas/6/1/6/gato-selkirk-rex_616_0_orig.jpg'),
 (45, 'Nebelung', 'Alegre lindo afavel', 'https://static1.patasdacasa.com.br/articles/3/27/33/@/11596-o-gato-nebelung-e-um-companheiro-leal-e-articles_media_mobile-2.jpg'),
-(46, 'Cornish', 'Docil carinhoso amavel', 'https://static1.patasdacasa.com.br/articles/5/27/55/@/11648-a-raca-de-gato-cornish-rex-e-uma-das-pou-articles_media_mobile-2.jpg'),
+(46, 'Cornish', 'Dócil carinhoso amável', 'https://static1.patasdacasa.com.br/articles/5/27/55/@/11648-a-raca-de-gato-cornish-rex-e-uma-das-pou-articles_media_mobile-2.jpg'),
 (47, 'Ocicat', 'Carinhoso afetuoso', 'https://image.cachorrogato.com.br/thumb/500/500/1/imagens/racas/imagem323.jpg'),
 (48, 'Peterbald', 'Carinhoso calmo', 'https://image.cachorrogato.com.br/thumb/500/500/1/imagens/racas/imagem329.jpg'),
-(49, 'Selvagem', 'Solitario tranquilo territorial', 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Felis_silvestris_silvestris.jpg/280px-Felis_silvestris_silvestris.jpg'),
+(49, 'Selvagem', 'Solitário tranquilo territorial', 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Felis_silvestris_silvestris.jpg/280px-Felis_silvestris_silvestris.jpg'),
 (50, 'Exotico de pelo curto', 'Familiares carinhosos', 'https://t2.ea.ltmcdn.com/pt/razas/7/0/6/gato-exotico-de-pelo-curto_607_0_orig.jpg'),
 (51, 'Azul russo', 'Tranquilos carinhosos', 'https://www.petlove.com.br/images/breeds/193039/profile/original/azul_russo-p.jpg?1532625892'),
-(52, 'Scottish fold', 'Tranquilo sociavel amoroso', 'https://i0.wp.com/www.portaldodog.com.br/cachorros/wp-content/uploads/2022/01/CATSTREET9.jpg?resize=1080%2C1080&ssl=1'),
-(53, 'Snowshoe', 'Tranquilo pacifico', 'https://www.petlove.com.br/images/breeds/228275/profile/original/GettyImages-506509770.jpg?1598892401'),
+(52, 'Scottish fold', 'Tranquilo sociável amoroso', 'https://i0.wp.com/www.portaldodog.com.br/cachorros/wp-content/uploads/2022/01/CATSTREET9.jpg?resize=1080%2C1080&ssl=1'),
+(53, 'Snowshoe', 'Tranquilo pacífico', 'https://www.petlove.com.br/images/breeds/228275/profile/original/GettyImages-506509770.jpg?1598892401'),
 (54, 'Manx', 'Equilibrado carinhoso', 'https://image.cachorrogato.com.br/thumb/800/600/1/imagens/racas/imagem314.jpg'),
 (55, 'Angora turco', 'Esbelto elegante', 'https://www.petlove.com.br/images/breeds/193065/profile/original/angora-p.jpg?1532625875'),
-(56, 'Bombaim', 'Sociavel carinhoso', 'https://www.petlove.com.br/images/breeds/225620/profile/original/bombay-gato-petlove.jpg?1595269194'),
+(56, 'Bombaim', 'Sociável carinhoso', 'https://www.petlove.com.br/images/breeds/225620/profile/original/bombay-gato-petlove.jpg?1595269194'),
 (57, 'Noruegues da floresta', 'Belo pelagem longa', 'https://static1.patasdacasa.com.br/articles/1/33/31/@/16430-o-noruegues-da-floresta-e-um-gato-grande-opengraph_1200-2.jpg'),
 (58, 'Siberiano', 'Carinhoso robusto', 'https://royalpets.vteximg.com.br/arquivos/ids/177819/gato-siberiano-320x250.jpg?v=637534817463970000'),
 (59, 'Bengal', 'Carinhoso hiperativo', 'https://static1.patasdacasa.com.br/articles/6/80/6/@/16646-o-gato-bengal-e-uma-raca-que-parece-um-l-opengraph_1200-2.jpg'),
 (60, 'Siames', 'Leais expressivos lindos', 'https://www.petlove.com.br/images/breeds/192825/profile/original/siames-p.jpg?1532626975'),
 (61, 'Ashera', 'Calmo silencioso', 'https://guiaanimal.net/uploads/content/image/54425/Design_sem_nome__32_.png'),
-(62, 'Munchkin', 'Amavel docil inteligente', 'https://static1.patasdacasa.com.br/articles/3/16/33/@/16431-o-munchkin-e-inconfundivel-as-patinhas-articles_media_mobile-2.jpg'),
-(63, 'Sphynx', 'Doce pacifico', 'https://www.petlove.com.br/images/breeds/192403/profile/original/sphynx-p.jpg?1532626992'),
+(62, 'Munchkin', 'Amável dócil inteligente', 'https://static1.patasdacasa.com.br/articles/3/16/33/@/16431-o-munchkin-e-inconfundivel-as-patinhas-articles_media_mobile-2.jpg'),
+(63, 'Sphynx', 'Doce pacífico', 'https://www.petlove.com.br/images/breeds/192403/profile/original/sphynx-p.jpg?1532626992'),
 (64, 'Mau egipcio', 'Elegante possessivo', 'https://www.petlove.com.br/images/breeds/226085/profile/original/mau-egipcio-banner.jpg?1595974266'),
-(65, 'Himalaio', 'Elegante inteligente simpatico', 'https://www.petlove.com.br/images/breeds/223870/profile/original/perfil-himalaio.jpg?1595617107'),
+(65, 'Himalaio', 'Elegante inteligente simpático', 'https://www.petlove.com.br/images/breeds/223870/profile/original/perfil-himalaio.jpg?1595617107'),
 (66, 'Havana', 'Ativo carinhoso', 'https://www.petlove.com.br/images/breeds/225906/mobile_header/original/GettyImages-1255873048.jpg?1595521839'),
 (67, 'Europeu', 'Inteligente independente', 'https://petfriendsworld.pt/wp-content/uploads/2020/04/gato-europeu-comum1.jpg'),
 (68, 'Balines', 'Bonito suave', 'https://www.zooplus.pt/magazine/wp-content/uploads/2020/03/gato-balines-768x512.jpeg'),
-(69, 'Mist australiano', 'Brincalhao amavel simpatico', 'https://image.cachorrogato.com.br/thumb/800/600/1/imagens/racas/imagem268.jpg');
+(69, 'Mist australiano', 'Brincalhão amável simpático', 'https://image.cachorrogato.com.br/thumb/800/600/1/imagens/racas/imagem268.jpg');
 
 --
 -- Índices para tabelas despejadas
